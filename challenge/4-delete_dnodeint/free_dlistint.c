f75605c7a14ccbaaae9bf8c9aa456ba7f51bb724
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-08-09 22:29:52 +0000

    task 4: Add all helper files and fix delete_dnodeint_at_index

---
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+    dlistint_t *next;
+
+    current = head;
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
