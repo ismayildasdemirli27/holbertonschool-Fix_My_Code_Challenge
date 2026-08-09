2ba96efe247af396a6db74d5a0afb93b485c6839
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-08-09 22:40:30 +0000

    fix task 4: final correct implementation of delete_dnodeint

---
+++ delete_dnodeint_at_index.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
+ * @head: Double pointer to the head of the list.
+ * @index: Index of the node that should be deleted.
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current = *head;
+    unsigned int i = 0;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    if (index == 0)
+    {
+        *head = (*head)->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+
+    while (current != NULL && i < index)
+    {
+        current = current->next;
+        i++;
+    }
+
+    if (current == NULL)
+        return (-1);
+
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+
+    free(current);
+    return (1);
+}
