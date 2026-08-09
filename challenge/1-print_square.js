3b6347cff28326fe24dfe40963dd56d2d67c1368
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-08-09 22:03:21 +0000

    task 1: Fix print square implementation in JavaScript

---
+++ challenge/1-print_square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+* Print a square of size $size with character #
+*/
+const size = parseInt(process.argv[2]);
+
+if (!isNaN(size) && size > 0) {
+    for (let i = 0; i < size; i++) {
+        console.log('#'.repeat(size));
+    }
+}
